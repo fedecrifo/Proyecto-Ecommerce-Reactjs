2176fd92d0ba1d1d69615afb422ba14653767f4f
fedecrifo <fedecrifo@gmail.com>
2022-11-12 09:12:03 -0300

    probando deploy

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -117,7 +117,7 @@ import { useNavigate } from "react-router-dom"
             />
           </div>
           <div className="buttons-order">
-            <button class="btn-6" onClick={createOrder}><span>Generar Pedido</span></button>
+            <button class="btn-6" onClick={createOrder}><span>Generar pedido</span></button>
     
           </div>
         </div>
